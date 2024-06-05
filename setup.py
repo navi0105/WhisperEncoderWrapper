c518c230557f831f4bb8718cae5be60b777550cd
navi0105 <ivan000105@gmail.com>
2024-06-05 22:35:03 +0800

    add setup & testing scripts

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="WhisperEncoderWrapper",
+    description="A simple wrapper of OpenAI Whisper's encoder model.",
+    version="0.0.1",
+    author="Ivan Leong",
+    author_email="ivan000105@gmail.com",
+    readme="README.md",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    license="MIT",
+    url="",
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    install_requires=[
+        'torch',
+        'tqdm',
+        'openai-whisper'
+    ],
+    extras_require={"dev": ["pytest"]}
+)
---
+++ tests/test_wrapper.py
@@ -0,0 +1,14 @@
+import torch
+from wrapper.wrapper import WhisperEncoderWrapper
+
+def test_load_wrapper():
+    encoder = WhisperEncoderWrapper('base', 'cpu')
+    
+def test_forward():
+    encoder = WhisperEncoderWrapper('base', 'cpu')
+    
+    x = torch.randn(16, 80, 3000)
+    with torch.no_grad():
+        y = encoder(x)
+
+    assert y.shape == (16, 1500, 512)
